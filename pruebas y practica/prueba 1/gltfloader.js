c4f746150805f370158449813b9a4c6ce1dfde9e
Benja.dev <benja.dev.benjavaldez2812@gmail.com>
2026-05-26 21:56:06 -0300

    limpio repositorio

--- pruebas y practica/prueba 1/gltfloader.js
+++
@@ -1,4 +0,0 @@
-// Carga GLTFLoader desde CDN
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/js/loaders/GLTFLoader.js';
-document.head.appendChild(script);
